8eea0bd3a80b34d66bdc5ef2600bcc0be06ce714
maliibun <malinaroxana17@gmail.com>
2026-03-08 20:24:49 +0200

    final

--- src/contact/components/componente_echipa/componenta_echipa.js
+++ src/contact/components/componente_echipa/componenta_echipa.js
@@ -5,17 +5,14 @@ import Sergiu from '../../images/tz1AUTt - Imgur 8.svg';
 function ComponentaEchipa() {
     return (
         <div className="ComponentaEchipa">
-        <div className="Echipa">
-            <h1>Echipa</h1>
-        </div>
-            <div className="Sergiu">
-            <img src={Sergiu} alt="Sergiu"/>
-            </div>
             <div className="Arrow1">
                 <img src={Arrow} alt="Arrow" />
             </div>
-            <div className="Coordonator"><h2>Coordonator</h2></div>
-            <div className="Sergiu1"><h1>Sergiu Adrian Preda</h1></div>
+            <div className="Sergiu">
+            <img src={Sergiu} alt="Sergiu"/>
+            </div>
+            <div className="Sergiu1">Sergiu Adrian Preda</div>
+            <div className="Coordonator">Coordonator</div>
       </div>
     );
 }
--- src/contact/index.js
+++ src/contact/index.js
@@ -4,7 +4,7 @@ import PersoanaContact from './components/persoana_contact/persoana_contact';
 function Contact(){
     return(
         <div className="header">
-            <h1 className="header_title">Contact</h1>
+            <h1 className="header_title">Contact    </h1>
             <PersoanaContact />
             <PersoanaContact />
         </div>
