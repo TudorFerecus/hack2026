a884dabfcefa6f3018b79e8b9b031fdea34594ef
maliibun <malinaroxana17@gmail.com>
2026-03-08 15:52:45 +0200

    added component

---
+++ src/contact/components/persoana_contact/persoana_contact.js
@@ -0,0 +1,11 @@
+import './persoana_contact.css';
+
+function PersoanaContact(){
+    return (
+        <div>
+            <h1>Nume Persoana</h1>
+        </div>
+    );
+}
+
+export default PersoanaContact;
--- src/contact/index.js
+++ src/contact/index.js
@@ -1,9 +1,11 @@
 import './index.css';
+import PersoanaContact from './components/persoana_contact/persoana_contact';
 
 function Contact(){
     return(
         <div>
             <h1>Contact</h1>
+            <PersoanaContact />
         </div>
     )
 }
