ad4d93c7970984f966d27e30348ee3fa64c21aa9
vlad <cazac46@gmail.com>
2026-03-08 16:37:09 +0200

    nici nu stiu

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
-import './App.css';
+import './Home.css';
+import banner from "./home/Logo EESTEC negru cu guidelines.svg";
+import panc1 from "./home/panc1.svg";
+import panc2 from "./home/panc2.svg";
+import panc3 from "./home/panc3.svg";
+import bat from "./home/bat.svg";
+import elipsa from "./home/Ellipse 2.svg";
+import navlogo from "./home/navsvg.svg";
+
 
 function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <img src="Logo EESTEC negru cu guidelines.svg" id="banner" />
+        <navbar>
+            <button type=""><img src={navlogo} id="navlogo" /></button>
+        </navbar>
+      </section>
+      <section id="">
+        <img src={panc1} class="panc1" />
+        <img src={panc2} class="panc2" />
+        <img src={panc3} class="panc3" />
+        <img src={bat} class="batpoza" />
+        <img id="maxlim" src={elipsa} class="elipsa" />
+        <p class="text1">25-26 Martie</p>
+        <p class="text2">Automatica si Calculatoare UNSTPB</p>
+        <p class="text3">fair.eestec.ro</p>
+
+        <p>Timp ramas pana la eveniment:</p>
+        <p>TIMP</p>
+        <button id="aplica" name="aplica" type=""><a href="#">APLICA</a></button>
+      </section>
+      <h1 id="program-banner"></h1>
+      <section>
+        <h2>Marti, 25 martie</h2>
+        <p>10:00<br/>Deschidere</p>
+        <p>10:00<br/>Conferinta</p>
+        <p>10:00<br/>TBA</p>
+        <p>10:00<br/>TBA</p>
+      </section>
+      <section>
+        <h2>Miercuri, 26 martie</h2>
+        <p>10:00<br/>Deschidere</p>
+        <p>10:00<br/>Conferinta</p>
+        <p>10:00<br/>TBA</p>
+        <p>10:00<br/>TBA</p>
+      </section>
     </div>
   );
 }
