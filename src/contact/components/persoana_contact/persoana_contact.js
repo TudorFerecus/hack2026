aca5892ebe82802d34c0622da4b3c815349e6a04
Sergiuxszz <sergiu.baroian@eestec.ro>
2026-03-08 18:19:42 +0200

    blabla

--- src/contact/components/persoana_contact/persoana_contact.js
+++ src/contact/components/persoana_contact/persoana_contact.js
@@ -2,23 +2,13 @@ import './persoana_contact.css';
 
 function PersoanaContact() {
     return (
-            <div className="chenar">
-            <div className="titlu">
-                <h1>Sergiu Adrian Preda</h1>
-            </div>
-         
-        <div className="info">
-        <u1>
-         presedinte EESTEC Bucuresti
-         </u1>
-         </div>
-         <div className="info1">
-         <u1>+0707070707</u1>
-         </div>
-         <div className="info2">
-         <u1>sergiu.preda@eestec.ro </u1>
-        </div>
-        </div>
+        <div class="contact-card">
+        <div class="profile-image"></div>
+        <h2 class="name">Sergiu Adrian Preda</h2>
+        <p class="role">presedinte EESTEC Bucuresti</p>
+        <p class="phone">+0707070707</p>
+        <p class="email">sergiu.preda@eestec.ro</p>
+      </div>
     );
 }
 
