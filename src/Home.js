0f8f5f92598649dfd2da6ba9dbdaeb6358be645b
bolba-cosmin <bolbacosmin342@gmail.com>
2026-03-08 17:52:15 +0200

    17:52 ff multe adaugari

--- src/Home.js
+++ src/Home.js
@@ -7,8 +7,35 @@ import panc3 from "./home/panc3.svg";
 import bat from "./home/bat.svg";
 import elipsa from "./home/Ellipse 2.svg";
 import navlogo from "./home/navsvg.svg";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const [timeLeft, setTimeLeft] = useState({
+    days: "00",
+    hours: "00",
+    minutes: "00"
+  });
+
+  useEffect(() => {
+    const eventDate = new Date("2026-04-01T18:00:00").getTime();
+
+    const interval = setInterval(() => {
+      const now = Date.now();
+      const diff = eventDate - now;
+
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / (1000 * 60)) % 60);
+
+      setTimeLeft({
+        days: String(days).padStart(2, "0"),
+        hours: String(hours).padStart(2, "0"),
+        minutes: String(minutes).padStart(2, "0")
+      });
+    }, 1000);
+
+  return () => clearInterval(interval);
+  }, []);
   return (
     <div className="App">
       <section>
@@ -26,13 +53,15 @@ function Home() {
         </div>
         <img id="maxlim" src={elipsa} class="elipsa" />
         <p class="text1">25-26 Martie</p>
-        <p class="text2">Automatica si Calculatoare<br/>UNSTPB</p>
+        <p class="text2">Automatica si<br/>Calculatoare<br/>UNSTPB</p>
         <p class="text3">fair.eestec.ro</p>
 
-        <p>Timp ramas pana la eveniment:</p>
-        <span id="days">00</span>d |
-        <span id="hours">00</span>h |
-        <span id="minutes">00</span>m
+        <p class="timpramas">Timp ramas pana la eveniment:</p>
+        <div class="ddhhmm">
+        <span>{timeLeft.days}</span>d |
+        <span>{timeLeft.hours}</span>h |
+        <span>{timeLeft.minutes}</span>m
+        </div>
         <button id="aplica" name="aplica" type=""><a href="#">APLICA</a></button>
       </section>
       <h1 id="program-banner"></h1>
