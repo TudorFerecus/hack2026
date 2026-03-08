e043b60d4a3db3a1e3228c84c3e9d82772401249
Teona <teona.antonia06@gmail.com>
2026-03-08 15:47:11 +0200

    setup and background color

---
+++ src/about-page/About.js
@@ -0,0 +1,15 @@
+import './About.css';
+
+function About() {
+  return (
+    <div className="About">
+        <div className="background">
+            <div className="parent-container">
+
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default About;
